729920f13434051d1cb8545d02b7a8e61e6a7a1f
AmeltonramsGit5 <cse25-319@thuto.bac.ac.bw>
2026-05-16 13:37:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,113 @@
-const products = [
-    {
-        name: "Black Hoodie",
-        price: "P550",
-        image: "images/hoodie1.jpg"
-    },
-    {
-        name: "Graphic Tee",
-        price: "P300",
-        image: "images/tee1.jpg"
-    },
-    {
-        name: "Cargo Pants",
-        price: "P700",
-        image: "images/cargo1.jpg"
+const products = {
+    men: {
+        hoodies: [
+            {
+                name: "Black Hoodie",
+                price: "P550",
+                badge: "Bestseller",
+                image: "images/hoodie1.jpg"
+            },
+            {
+                name: "Grey Hoodie",
+                price: "P600",
+                badge: "",
+                image: "images/hoodie2.jpg"
+            },
+            {
+                name: "Oversized Hoodie",
+                price: "P650",
+                badge: "New",
+                image: "images/hoodie3.jpg"
+            }
+        ],
+
+        cargo: [
+            {
+                name: "Black Cargo",
+                price: "P700",
+                badge: "New",
+                image: "images/cargo1.jpg"
+            },
+            {
+                name: "Khaki Cargo",
+                price: "P750",
+                badge: "",
+                image: "images/cargo2.jpg"
+            },
+            {
+                name: "Baggy Cargo",
+                price: "P800",
+                badge: "",
+                image: "images/cargo3.jpg"
+            }
+        ],
+
+        tees: [
+            {
+                name: "Graphic Tee",
+                price: "P300",
+                badge: "Hot",
+                image: "images/tee1.jpg"
+            },
+            {
+                name: "Vintage Tee",
+                price: "P350",
+                badge: "",
+                image: "images/tee2.jpg"
+            },
+            {
+                name: "Street Tee",
+                price: "P400",
+                badge: "",
+                image: "images/tee3.jpg"
+            }
+        ]
     }
-];
+};
 
-function renderProducts(containerId) {
-    const container = document.getElementById(containerId);
+function createProductCard(product) {
+    return `
+        <div class="product-card">
 
-    if (!container) return;
-
-    container.innerHTML = products.map(product => `
-        <div class="col-md-4">
-            <div class="card shadow-sm">
-                <img src="${product.image}" class="card-img-top">
-                <div class="card-body text-center">
-                    <h5>${product.name}</h5>
-                    <p>${product.price}</p>
-                </div>
+            <div style="position:relative;">
+                ${product.badge ? `<span class="badge-custom">${product.badge}</span>` : ""}
+                <img src="${product.image}" class="product-img" alt="${product.name}">
             </div>
+
+            <div class="product-info">
+                <div class="product-name">${product.name}</div>
+                <div class="product-price">${product.price}</div>
+                <button class="add-btn">+</button>
+            </div>
+
         </div>
-    `).join("");
+    `;
 }
 
-renderProducts("homeFeaturedGrid");
-renderProducts("shopGrid");
+function renderLatestDrops() {
+    const homeGrid = document.getElementById("homeFeaturedGrid");
+
+    if(homeGrid){
+        const latest = [
+            products.men.hoodies[0],
+            products.men.cargo[0],
+            products.men.tees[0]
+        ];
+
+        homeGrid.classList.add("products-grid");
+        homeGrid.innerHTML = latest.map(createProductCard).join("");
+    }
+}
+
+function renderShop(category="hoodies") {
+    const shopGrid = document.getElementById("shopGrid");
+
+    if(shopGrid){
+        shopGrid.classList.add("products-grid");
+        shopGrid.innerHTML =
+            products.men[category].map(createProductCard).join("");
+    }
+}
+
+renderLatestDrops();
+renderShop();
